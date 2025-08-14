f95dff94c74683c6274242f9eefbcd5e9a39a755
Nandini Bollineni <bollineninavanandini@gmail.com>
2025-08-14 06:52:26 +0000

    Initial Commit

---
+++ git/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("git.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
